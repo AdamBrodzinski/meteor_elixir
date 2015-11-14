2b33cb268e59bd6c48ea81a94ff8620754a5f4b2
Adam Brodzinski <adambrodzinski@gmail.com>
2015-11-14 14:53:14 -0500

    adds phoenix JS runtime

---
+++ chatroom/packages/phoenix/package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  name: 'phoenix-runtime',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.export('PhoenixRuntime');
+  api.addFiles('phoenix.js');
+});
