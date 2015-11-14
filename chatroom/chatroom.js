c1035b9e8514a9d481a1b82350afd0b5f5767dcb
Adam Brodzinski <adambrodzinski@gmail.com>
2015-11-14 15:20:23 -0500

    adds minimum wiring to talk to phoenix
    connects and logs any incoming messages sent into
    chatroom by any other user

--- chatroom/chatroom.js
+++ chatroom/chatroom.js
@@ -1,6 +1,26 @@
 Chats = new Mongo.Collection('chats');
 
 if (Meteor.isClient) {
+  /* Wire up Phoenix on startup */
+  const {Socket} = PhoenixRuntime;
+  const socket = new Socket("ws://localhost:4000/socket")
+
+  // TODO add authentication with Meteor `_loginToken`
+  socket.connect(/* {token: window.userToken} */)
+
+  // join the rooms channel with a topic of 'lobby' (public)
+  const channel = socket.channel("rooms:lobby", {})
+
+  channel.join()
+  .receive("ok",    resp => console.log("Joined successfully", resp))
+  .receive("error", resp => console.log("Unable to join",      resp))
+
+  channel.on("new_msg", p => {
+    console.log('new message', p);
+  })
+  /* End Phoenix wiring */
+
+
   Template.chats.helpers({
     chatData: function() {
       return Chats.find({}, {sort: {time: 1}});
@@ -26,6 +46,11 @@ if (Meteor.isClient) {
     myDiv.animate({ scrollTop: myDiv.prop("scrollHeight") - myDiv.height() }, 0);
   })
 }
+// END isClient
+
+
+
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
