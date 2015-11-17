8a890cb6541af630d8f2c06109401d10093194a0
Adam Brodzinski <adambrodzinski@gmail.com>
2015-11-16 19:32:13 -0500

    use url instead of other keys
    using a remote db is easier if we just pass in the url key instead.
    adds hardcoded secret base back in.

--- elixir_server/config/prod.secret.exs
+++ elixir_server/config/prod.secret.exs
@@ -4,15 +4,7 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :chatty, Chatty.Endpoint,
-  #secret_key_base: "MeDuhccgUDkkBmBTcXufvsefYI8dWcHJUkIq+MY17om97qM1VIQjMjktFPKEVsq9"
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: "MeDuhccgUDkkBmBTcXufvsefYI8dWcHJUkIq+MY17om97qM1VIQjMjktFPKEVsq9"
 
 config :chatty, Chatty.Repo,
-  database: "chatty",
-  username: "testuser",
-  password: "testpassword",
-  #hostname: "candidate.16.mongolayer.com:11213,candidate.57.mongolayer.com"
-  hostname: "candidate.16.mongolayer.com",
-  port: "11213"
-  #hostname: System.get_env("DATABASE_HOST")
-
+  url: "mongodb://testuser:testpassword@candidate.16.mongolayer.com:11213,candidate.57.mongolayer.com:10138/chatty?replicaSet=set-56489fdcbab49bb86e000bfb"
